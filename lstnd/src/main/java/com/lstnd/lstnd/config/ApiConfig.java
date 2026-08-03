478d541414643d0caf4757cad5c6cc15e7289e8b
mateusgromowski <mateusbarbosagromowski@gmail.com>
2026-08-03 14:16:38 -0300

    chore: finalizada classe de configuração

---
+++ lstnd/src/main/java/com/lstnd/lstnd/config/ApiConfig.java
@@ -0,0 +1,19 @@
+package com.lstnd.lstnd.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class ApiConfig {
+    @Bean
+    public RestClient restClientRequest() {
+        return RestClient.builder().baseUrl("https://api.spotify.com/v1/").build();
+    }
+
+    @Bean
+    public RestClient restClientAuth() {
+        return RestClient.builder().baseUrl("https://accounts.spotify.com/api/token")
+                .defaultHeader("Content-Type: application/x-www-form-urlencoded").build();
+    }
+}
