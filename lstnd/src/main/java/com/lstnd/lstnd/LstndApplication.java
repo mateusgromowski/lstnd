125043f81425bfd07d184c817f0cb6ddf4da2776
mateusgromowski <mateusbarbosagromowski@gmail.com>
2026-08-03 14:07:36 -0300

    chore: configuração inicial do projeto

---
+++ lstnd/src/main/java/com/lstnd/lstnd/LstndApplication.java
@@ -0,0 +1,13 @@
+package com.lstnd.lstnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LstndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LstndApplication.class, args);
+	}
+
+}
---
+++ lstnd/src/test/java/com/lstnd/lstnd/LstndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lstnd.lstnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LstndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
