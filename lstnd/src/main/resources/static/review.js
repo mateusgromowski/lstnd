5cb7b5af3c566e74b4290c0d0eb02ec11cba9f26
mateusgromowski <mateusbarbosagromowski@gmail.com>
2026-08-08 01:40:28 -0300

    style: estilizada informações do álbum

--- lstnd/src/main/resources/static/review.js
+++ lstnd/src/main/resources/static/review.js
@@ -1,25 +1,39 @@
 const albumId = new URLSearchParams(window.location.search).get("id");
+
 showAlbum();
-async function showAlbum(){
+
+async function showAlbum() {
     const url = `/spotify/albums/${albumId}`;
+
     try {
         const response = await fetch(url);
-        if(!response.ok){
+
+        if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
+
         const responseJson = await response.json();
+
         printAlbum(responseJson);
+
     } catch (error) {
         console.error(error.message);
     }
 }
 
-function printAlbum(album){
-    let albumInfo = document.getElementById("albumInfo");
+function printAlbum(album) {
+    const albumInfo = document.getElementById("albumInfo");
+
     albumInfo.innerHTML = `
-        <img src="${album.capeUrl}">
-        <h3>${album.title}</h3>
-        <p>${album.author}</p>
-        <p>${album.releaseDate}</p>
-    `
+        <img class="album-cover" src="${album.capeUrl}" alt="Capa do álbum">
+
+        <div class="album-details">
+            <h1>${album.title}</h1>
+
+            <div class="album-meta">
+                <p>${album.author}</p>
+                <p>${album.releaseDate}</p>
+            </div>
+        </div>
+    `;
 }
