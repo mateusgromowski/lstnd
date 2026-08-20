9037f1815bd72f5d3cc5a68718f97e980df2fc0f
mateusgromowski <mateusbarbosagromowski@gmail.com>
2026-08-20 14:33:17 -0300

    test: testada funcionalidade de retorno de todas as reviews de um id específico

---
+++ lstnd/src/test/java/com/lstnd/lstnd/service/ReviewServiceTest.java
@@ -0,0 +1,38 @@
+package com.lstnd.lstnd.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.lstnd.lstnd.model.Review;
+import com.lstnd.lstnd.repository.ReviewRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ReviewServiceTest {
+    @Mock
+    ReviewRepository repository;
+
+    @InjectMocks
+    ReviewService service;
+
+    @Test
+    void shouldReturnAllReviewsTest() {
+        List<Review> reviews = List.of(
+                Review.builder().id(1L).userName("peter").review("muito bom").score(5).build(),
+                Review.builder().id(1L).userName("retep").review("muito ruim").score(0).build());
+        when(repository.findAllBySpotifyId("abc123")).thenReturn(reviews);
+        List<Review> serviceReviews = service.getAllReviews("abc123");
+        assertEquals(2, serviceReviews.size());
+        assertEquals(reviews.get(0), serviceReviews.get(0));
+        assertEquals(reviews.get(1), serviceReviews.get(1));
+        verify(repository).findAllBySpotifyId("abc123");
+    }
+}
